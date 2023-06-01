1bd8d7ac0745074c920d95735784e116a58bacea
esubalew-gosaye <mesaye2010@gmail.com>
2023-06-01 08:54:20 +0300

    folder structure and rename.

--- Telehakim/wsgi.py
+++ Telehakim/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Telehakimfinal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Telehakim.settings')
 
 application = get_wsgi_application()
--- payment/apps.py
+++ payment/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PayConfig(AppConfig):
+class PaymentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pay'
+    name = 'payment'
